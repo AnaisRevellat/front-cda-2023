e1954fb65dc3bf3726f07ce3ec434def4a3acd82
AnaisRevellat <anaisrevellat@gmail.com>
2023-06-29 22:08:29 +0200

    Routing

--- creat-and-broc/src/App.js
+++ creat-and-broc/src/App.js
@@ -1,59 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+
 import './App.styles.scss';
 
-import Directory from './components/directory/directory.component';
+import Home from './routes/home/home.component';
 
 const App = () => {
-
-  const homeUrl = 'https://zupimages.net/up/23/26/yv4t.png';
-
-  const categories = [
-    {
-      id: 1,
-      title: 'Art de la table',
-      subtitle: 'Inspiration',
-      backUrl: 'https://zupimages.net/up/23/26/pq25.png',
-    },
-    {
-      id: 2,
-      title: 'Décorer',
-      subtitle: 'Inspiration',
-      backUrl: 'https://zupimages.net/up/23/26/cb98.png',
-    },
-    {
-      id: 3,
-      title: 'Luminaires',
-      subtitle: 'Inspiration',
-      backUrl: 'https://zupimages.net/up/23/26/i6zs.png',
-    },
-    {
-      id: 4,
-      title: 'Textile',
-      subtitle: 'Inspiration',
-      backUrl: 'https://zupimages.net/up/23/26/wldy.png',
-    },
-    {
-      id: 5,
-      title: 'Mobilier',
-      subtitle: 'Inspiration',
-      backUrl: 'https://zupimages.net/up/23/26/jvpr.png',
-    }
-  ]
-
   return (
-    <>
-      <div className="home-container">
-
-        <main className="main-container">
-          <div className="home-category-container">
-            <div className="background-home" style={{ backgroundImage: `url(${homeUrl})` }} />
-          </div>
-        </main>
-
-        <Directory categories={categories} />
-
-       
-      </div>
-    </>
+    <Routes >
+      <Route path='/' element={<Home />} />      
+    </Routes>
   );
 }
 
--- creat-and-broc/src/components/directory/directory.component.jsx
+++ creat-and-broc/src/components/directory/directory.component.jsx
@@ -1,15 +1,25 @@
+import CategoryItem from "../category-item/category-item.component";
 
-import CategoryItem from "../category-item/category-item.component"
+import "./directory.styles.scss";
 
-import './directory.styles.scss'
+const Directory = ({ categories, homeUrl }) => {
+	return (
+		<>
+			<main className='main-container'>
+				<div className='home-category-container'>
+					<div
+						className='background-home'
+						style={{ backgroundImage: `url(${homeUrl})` }}
+					/>
+				</div>
+			</main>
 
-const Directory = ({categories}) => {
-  return (
-    <section className="directory-container">
-    {categories.map((category) => (
-      <CategoryItem key={category.id} category={category} />
-    ))}
-  </section>
-  )
-}
+			<section className='directory-container'>
+				{categories.map((category) => (
+					<CategoryItem key={category.id} category={category} />
+				))}
+			</section>
+		</>
+	);
+};
 export default Directory;
