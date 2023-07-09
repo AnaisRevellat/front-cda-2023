3a5e49d19b4c8c9a74c96cb34fbf3e6e656ec696
AnaisRevellat <anaisrevellat@gmail.com>
2023-07-09 09:58:53 +0200

    setting svg shopping bag colors

--- creat-and-broc/src/routes/navigation/navigation.component.jsx
+++ creat-and-broc/src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,9 @@
 import { useContext } from "react";
-
 import { Outlet, Link } from "react-router-dom";
 
+//components
 import { ReactComponent as CreatBrocLogo } from "../../../src/assets/creat_and_broc_logo.svg";
-
+import CartIcon from '../../components/cart-icon/cart-icon.component'
 import "./navigation.styles.scss";
 
 //context
@@ -30,6 +30,7 @@ const Navigation = () => {
 					<Link className='nav-link' to='/shop'>
 						Boutique
 					</Link>
+					<CartIcon />
 
 					{currentUser ? (
 						<span className='nav-link' onClick={signOutHandler}>
