63c42bd55ecc0233b191d197e7189880e2f0ad30
AnaisRevellat <anaisrevellat@gmail.com>
2023-07-13 13:13:09 +0200

    commit before update

--- creat-and-broc/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ creat-and-broc/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,6 @@
-//component
+//components
 import Button from "../button/button.component";
+import cartItem from "../cart_item/cart-item.component";
 
 //scss
 import "./cart-dropdown.styles.scss";
@@ -7,7 +8,9 @@ import "./cart-dropdown.styles.scss";
 const CartDropdown = () => {
 	return (
 		<div className='cart-dropdown-container'>
-			<div className='cart-items'></div>
+			<div className='cart-items'>
+        {}
+      </div>
 			<Button>Voir le panier</Button>
 		</div>
 	);
--- creat-and-broc/src/contexts/cart.context.jsx
+++ creat-and-broc/src/contexts/cart.context.jsx
@@ -1,14 +1,37 @@
-import { createContext, useState } from 'react';
+import { createContext, useState } from "react";
+
+//receiving the cart item array
+const addCartItem = (cartItems, productToAdd) => {
+	/*find is used here to find an existing product if it exists
+  we use a boolean value thanks to the callback */
+	const existingCartItem = cartItems.find(
+		(cartItem) => cartItem.id === productToAdd.id
+	);
+
+	//handling the case of a new product coming with the quantity field
+	return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
 
 export const CartContext = createContext({
-  isCartOpen: false,
-  setIsCartOpen: () => {}
-})
+	isCartOpen: false,
+	setIsCartOpen: () => {},
+	cartItems: [],
+	addItemToCart: () => {},
+});
 
-export const CartProvider = ({children}) => {
-  const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = {isCartOpen, setIsCartOpen};
-return (
-  <CartContext.Provider value={value}>{children}</CartContext.Provider>
-)
-}
+export const CartProvider = ({ children }) => {
+	const [isCartOpen, setIsCartOpen] = useState(false);
+	const [cartItems, setCartItems] = useState([]);
+
+	//whenever a user click on add to cart
+	const addItemToCart = (productToAdd) => {
+		setCartItems(addCartItem(cartItems, productToAdd));
+	};
+
+	const value = { isCartOpen, setIsCartOpen };
+	return (
+		<CartContext.Provider value={value}>
+			{children}
+		</CartContext.Provider>
+	);
+};
